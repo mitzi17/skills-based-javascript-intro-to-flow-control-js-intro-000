2a4842be9a409b63bd58392f2270eb48470e5ef9
Mitzi Estrada <m_estrada17@live.com>
2019-10-23 19:02:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,9 @@
 function basicTeenager(age) {
+  if (age > 13 || age < 19) {
+    baicTeenager = You are a teenager!()
+  } else {
+    basicTeenager = You are not a teenager()}
 
-}
 
 function teenager(age) {
 
